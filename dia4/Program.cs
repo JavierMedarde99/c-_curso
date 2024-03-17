0611c813bcdb8de39171cd6774243d1a6f009437
JavierMedardeMata <32664879+JavierMedarde99@users.noreply.github.com>
2024-03-17 11:58:55 +0100

    dia4

---
+++ dia4/Program.cs
@@ -0,0 +1,150 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+namespace Dia4
+{
+    class Program
+    {
+        static int puntuacionRecord = 10;
+        static string jugadorRecord = "juan";
+        static void Main(String[] arg) {
+            Console.WriteLine("Ingrese la temperatura");
+            string input = Console.ReadLine();
+            int temperatura;
+
+            if(int.TryParse(input,out temperatura)){
+            temperatura = int.Parse(input);
+            //string resultado = temperatura<=15 ? "Hace mucho frio" : (temperatura>=16 && temperatura<=28 ? "Hace un clima agradable" : "Hace mucho calor");
+
+            if(temperatura <10){
+                Console.WriteLine("Abrigate!");
+            }
+            else if(temperatura == 10){
+                Console.WriteLine("Hace 10 grados de temperatura");
+            }
+            else if(temperatura >30){
+                Console.WriteLine("Hacen 30 grados, !Que calor¡");
+            }else {
+                Console.WriteLine("Esta calido");
+            }
+
+            }else{
+                Console.WriteLine("El valor ingresado no era un numero");
+            }
+            
+
+            //If anidados
+
+            bool esAdmin = false;
+            bool estaRegistrado = true;
+            string nombreDeUsuario = "";
+
+            Console.WriteLine("Por favor ingrese su nombre de usuario");
+            nombreDeUsuario = Console.ReadLine();
+            if(estaRegistrado){
+                Console.WriteLine("Hola usuario registrado");
+                if(nombreDeUsuario != ""){
+                    Console.WriteLine("Hola " + nombreDeUsuario);
+                    if(nombreDeUsuario.Equals("Admin")){
+                        Console.WriteLine("Hola Admin");
+                    }
+                }
+            }
+
+            if(estaRegistrado && nombreDeUsuario != "" && nombreDeUsuario.Equals("Admin")){
+                Console.WriteLine("Bienvenido Admin");
+            }
+
+            //desafio 1
+            Registro();
+
+            //switch
+
+            int autos = 1;
+            switch (autos)
+            {
+                case 0:
+                    Console.WriteLine("Usted no paga inpuestos");
+                    break;
+                case 1:
+                    Console.WriteLine("Usted paga 15 dolares");
+                    break;
+                case 2:
+                    Console.WriteLine("Usted paga 30 dolares");
+                    break;
+                default:
+                    Console.WriteLine("Usted paga 15 dolares por auto");
+                    break;
+            }
+
+            string piedraPapelTiejera = "piedra";
+            switch (piedraPapelTiejera)
+            {
+                case "piedra":
+                     Console.WriteLine("Piedra mata a tijera");
+                     break;
+                case "tijera":
+                     Console.WriteLine("Piedra mata a papel");
+                     break;
+                case "papel":
+                     Console.WriteLine("Piedra mata a piedra");
+                     break;
+                default:
+                     Console.WriteLine("ese elemento no forma parte del juego");
+                     break;
+            }
+
+            //ternarios
+            int temperatura1 = -5;
+            string estadoDeAgua;
+
+            /*
+            if(temperatura1<0){
+                estadoDeAgua = "solido";
+            } else{
+                estadoDeAgua="liquido";
+            }*/
+
+            
+            estadoDeAgua = temperatura1<0 ? "solido":(temperatura1>99 ? "gaseoso":"liquido");
+
+            Console.WriteLine("El estado del agua es " + estadoDeAgua);
+
+            Console.Read();
+        }
+
+        public static void Registro(){
+            Console.WriteLine("Por favor ingrese un nombre de usuario");
+            string nombreDeUsuarioRegistrado = Console.ReadLine();
+
+            Console.WriteLine("Por favor ingrese una Contraseña");
+            string ContraseniaRegistrado = Console.ReadLine();
+
+            Console.WriteLine("Usuario Registrado correctamente, porfavor logease");
+
+            Console.WriteLine("Por favor ingrese su nombre de usuario");
+            string nombreDeUsuarioLogin = Console.ReadLine();
+
+            Console.WriteLine("Por favor ingrese su Contraseña");
+            string Contrasenialogin = Console.ReadLine();
+
+            if(nombreDeUsuarioRegistrado.Equals(nombreDeUsuarioLogin) && ContraseniaRegistrado.Equals(Contrasenialogin)){
+                Console.WriteLine("bienvenido "+ nombreDeUsuarioLogin);
+            }else{
+                Console.WriteLine("Usuario incorrecto");
+            }
+        }
+
+        public static void Puntuacion(string nombreJugador, int puntuacion){
+            if(puntuacion>puntuacionRecord){
+                puntuacionRecord = puntuacion;
+                jugadorRecord = nombreJugador;
+                Console.WriteLine("La nueva puntuacion mas alta es "+puntuacion);
+                Console.WriteLine("La puntuacion mas alta fue lograda por "+ nombreJugador);
+            }else{
+                Console.WriteLine("La puntuacion mas alta de "+puntuacionRecord+" no se ha podido superar, y aun esta en manos de "+ jugadorRecord);
+            }
+        }
+    }
+}
--- dia4/obj/Debug/net8.0/dia4.AssemblyInfo.cs
+++ dia4/obj/Debug/net8.0/dia4.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("dia3")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("dia4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d394fc264c96313082d8e2ffca26116c0e5a512b")]
-[assembly: System.Reflection.AssemblyProductAttribute("dia3")]
-[assembly: System.Reflection.AssemblyTitleAttribute("dia3")]
+[assembly: System.Reflection.AssemblyProductAttribute("dia4")]
+[assembly: System.Reflection.AssemblyTitleAttribute("dia4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
